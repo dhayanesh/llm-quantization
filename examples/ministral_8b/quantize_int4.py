ca99ee9f456430c9a134d18f9dbec107a26556d8
Dhayaneshwar B <63561465+dhayanesh@users.noreply.github.com>
2026-02-14 15:57:34 -0500

    Add quantize_int4.py to examples/ministral_8b

---
+++ examples/ministral_8b/quantize_int4.py
@@ -0,0 +1,52 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+MODEL_ID = "ministral_8b"
+SAVE_DIR = MODEL_ID + "-INT4-W4A16"
+
+model = AutoModelForCausalLM.from_pretrained(
+    MODEL_ID,
+    device_map="auto",
+    torch_dtype="auto",
+)
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
+
+
+# prepare calibration data
+from datasets import load_dataset
+
+NUM_CALIBRATION_SAMPLES = 512
+MAX_SEQUENCE_LENGTH = 2048
+
+# Load and preprocess the dataset
+ds = load_dataset("HuggingFaceH4/ultrachat_200k", split="train_sft")
+ds = ds.shuffle(seed=42).select(range(NUM_CALIBRATION_SAMPLES))
+
+def preprocess(example):
+    return {"text": tokenizer.apply_chat_template(example["messages"], tokenize=False)}
+ds = ds.map(preprocess)
+
+def tokenize(sample):
+    return tokenizer(sample["text"], padding=False, max_length=MAX_SEQUENCE_LENGTH, truncation=True, add_special_tokens=False)
+ds = ds.map(tokenize, remove_columns=ds.column_names)
+
+# load model and quantize
+from llmcompressor import oneshot
+from llmcompressor.modifiers.quantization import GPTQModifier
+from llmcompressor.modifiers.smoothquant import SmoothQuantModifier
+
+recipe = GPTQModifier(targets="Linear", scheme="W4A16", ignore=["lm_head"])
+
+oneshot(
+    model=model,
+    recipe=recipe,
+    dataset=ds,
+    max_seq_length=MAX_SEQUENCE_LENGTH,
+    num_calibration_samples=NUM_CALIBRATION_SAMPLES,
+    output_dir=SAVE_DIR
+)
+
+# model.save_pretrained(SAVE_DIR, save_compressed=True)
+# tokenizer.save_pretrained(SAVE_DIR)
+
+print("Quantized model saved at:", SAVE_DIR)
